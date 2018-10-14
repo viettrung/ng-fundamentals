5b935e813fc0496216039e0f653172fda197a858
Trung DUONG <dg.viettrung@gmail.com>
2018-10-14 23:44:35 +0700

    #37

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/events-thumbnail.component";
 import { NavBarComponent } from "./nav/navbar.components";
+import { EventService } from "./events/shared/event.service";
 
 @NgModule({
   declarations: [
@@ -16,6 +17,8 @@ import { NavBarComponent } from "./nav/navbar.components";
   imports: [
     BrowserModule
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
+
 export class EventsAppModule { }
--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -6,7 +6,11 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     <div class="well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div>Time: {{event?.time}}</div>
+        <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
         <div>Price: \${{event?.price}}</div>
         <div [hidden]="!event?.location">
             <span>Location: {{event?.location?.address}}</span>
@@ -19,6 +23,8 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     </div> 
     `,
     styles: [`
+        .green {color: #003300 !important;}
+        .bold {font-weight: bold;}
         .thumbnail {min-height: 210px;}
         .pad-left { margin-left: 10px;}
         .well div {color: #bbb;}
@@ -27,4 +33,11 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 export class EventThumbnailComponent {
     @Input() event: any
+
+    getStartTimeStyle() {
+        if (this.event && this.event.time === '8:00 am') {
+            return {color: '#003300', 'font-weight': 'bold'}
+        }
+        return {}
+    }
 }
