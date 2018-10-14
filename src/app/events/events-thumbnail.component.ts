e89db72355692ee66d859d0e944d437ab68efda0
Trung DUONG <dg.viettrung@gmail.com>
2018-10-14 11:26:24 +0700

    #30

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -3,20 +3,23 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 @Component({
     selector: 'event-thumbnail',
     template: `
-    <div class="well hoverwell thumnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">Location: {{event.location.city}}, {{event.location.country}}</span>
+    <div class="well hoverwell thumbnail">
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{event?.price}}</div>
+        <div [hidden]="!event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">Location: {{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
-        <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
+        <div [hidden]="!event?.onlineUrl">
+            Online URL: {{event?.onlineUrl}}
+        </div>
         
     </div> 
     `,
     styles: [`
+        .thumbnail {min-height: 210px;}
         .pad-left { margin-left: 10px;}
         .well div {color: #bbb;}
     `]
@@ -24,15 +27,4 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 export class EventThumbnailComponent {
     @Input() event: any
-    @Output() eventClick = new EventEmitter()
-    
-    someProperty: any = 'some property'
-
-    handleClickMe() {
-        this.eventClick.emit(this.event.name);
-    }
-
-    logFoo() {
-        alert('foo');
-    }
 }
