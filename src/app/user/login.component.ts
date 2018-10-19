d7971d4d3fe5a617b76155e76c2b3aae98f6c629
Trung DUONG <dg.viettrung@gmail.com>
2018-10-19 23:04:14 +0700

    #59

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,7 +4,10 @@ import { Router } from "@angular/router";
 
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styles: [`
+        em {float: right; color: #E05C65; padding-left: 10px}
+    `]
 })
 
 export class LoginComponent {
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { userRoutes } from "./user.routes";
 import { ProfileComponent } from "./profile.component";
 import { LoginComponent } from "./login.component";
@@ -11,7 +11,8 @@ import { LoginComponent } from "./login.component";
     imports: [
         CommonModule,
         RouterModule.forChild(userRoutes),
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule
     ],
     declarations: [
         ProfileComponent,
